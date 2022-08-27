19b9544211cab1b7f23c632b3325a4f3d40edef8
djagielo <dariusz.jagielo@gmail.com>
2022-08-27 13:28:13 +0200

    Added missing actuator dependency - preventing startup in Kubernetes

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,6 +23,7 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	runtimeOnly("org.mariadb:r2dbc-mariadb")
 	runtimeOnly("org.mariadb.jdbc:mariadb-java-client")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
