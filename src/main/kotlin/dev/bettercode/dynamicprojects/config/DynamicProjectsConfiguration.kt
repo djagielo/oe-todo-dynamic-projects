428d37972a8972d17b4f64ce63d165ac0d1b7873
djagielo <dariusz.jagielo@gmail.com>
2022-11-05 21:57:24 +0100

    Cleanup

--- src/main/kotlin/dev/bettercode/dynamicprojects/config/DynamicProjectsConfiguration.kt
+++ src/main/kotlin/dev/bettercode/dynamicprojects/config/DynamicProjectsConfiguration.kt
@@ -2,11 +2,15 @@ package dev.bettercode.dynamicprojects.config
 
 import dev.bettercode.dynamicprojects.DynamicProjectsFacade
 import dev.bettercode.dynamicprojects.application.*
+import dev.bettercode.dynamicprojects.infra.api.DynamicProjectsRestHandler
 import dev.bettercode.dynamicprojects.infra.db.inmemory.InMemoryDynamicProjectRepository
 import dev.bettercode.dynamicprojects.infra.tasks.TasksClient
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.MediaType
+import org.springframework.web.reactive.function.server.coRouter
+import java.time.Duration
 
 @Configuration
 class DynamicProjectsConfiguration {
@@ -19,7 +23,10 @@ class DynamicProjectsConfiguration {
             return DynamicProjectsFacade(
                 inMemoryDynamicProjectRepository,
                 PredefinedDynamicProjectsService(inMemoryDynamicProjectRepository),
-                recalculationService = ProjectRecalculationService(inMemoryDynamicProjectRepository, tasksPort = tasksPort)
+                recalculationService = ProjectRecalculationService(
+                    inMemoryDynamicProjectRepository,
+                    tasksPort = tasksPort
+                )
             )
         }
 
@@ -57,8 +64,36 @@ class DynamicProjectsConfiguration {
     @Bean
     internal fun projectRecalculationService(
         dynamicProjectRepo: DynamicProjectRepository,
-        @Value("\${tasks.url}") tasksUrl: String
+        tasksClientProperties: TasksClientProperties
     ): ProjectRecalculationService {
-        return ProjectRecalculationService(dynamicProjectRepo, tasksPort = TasksClient(tasksUrl = tasksUrl))
+        return ProjectRecalculationService(
+            dynamicProjectRepo,
+            tasksPort = TasksClient(tasksClientProperties = tasksClientProperties)
+        )
+    }
+
+    @Bean
+    internal fun taskClientProperties(
+        @Value("\${tasksClient.url}") tasksUrl: String,
+        @Value("\${tasksClient.timeoutMs}") timeoutMs: Int
+    ): TasksClientProperties {
+        return TasksClientProperties(tasksUrl, Duration.ofMillis(timeoutMs.toLong()))
+    }
+
+    @Bean
+    fun dynamicProjectController(): DynamicProjectsRestHandler {
+        return DynamicProjectsRestHandler()
+    }
+
+    @Bean
+    fun routes(dynamicProjectsController: DynamicProjectsRestHandler) = coRouter {
+        accept(MediaType.APPLICATION_JSON).nest {
+//            POST("/api/users", restController::createUser)
+//            DELETE("/api/users", restController::deleteUser)
+//
+//
+//            POST("/api/devices", restController::createDevice)
+//            DELETE("/api/devices", restController::deleteDevice)
+        }
     }
 }
--- src/main/kotlin/dev/bettercode/dynamicprojects/infra/tasks/TasksClient.kt
+++ src/main/kotlin/dev/bettercode/dynamicprojects/infra/tasks/TasksClient.kt
@@ -3,6 +3,7 @@ package dev.bettercode.dynamicprojects.infra.tasks
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import dev.bettercode.dynamicprojects.application.TaskDto
 import dev.bettercode.dynamicprojects.application.TasksPort
+import dev.bettercode.dynamicprojects.config.TasksClientProperties
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.channelFlow
 import kotlinx.coroutines.reactor.awaitSingle
@@ -18,19 +19,19 @@ import org.springframework.web.reactive.function.client.WebClient
 @JsonIgnoreProperties(ignoreUnknown = true)
 private data class TasksList(val content: List<TaskDto>, val totalElements: Long)
 
-class TasksClient(private val webClient: WebClient = WebClient.create(), private val tasksUrl: String): TasksPort {
+class TasksClient(private val webClient: WebClient = WebClient.create(), private val tasksClientProperties: TasksClientProperties): TasksPort {
 
     override suspend fun getAllOpen(page: Pageable): Page<TaskDto> {
         return fetchPage(page.pageNumber, page.pageSize)
     }
 
     private suspend fun fetchPage(pageNo: Int, pageSize: Int): Page<TaskDto> {
-        return webClient.get().uri("${tasksUrl}?page=${pageNo}&size=${pageSize}")
+        return webClient.get().uri("${tasksClientProperties.tasksClientUrl}?page=${pageNo}&size=${pageSize}")
             .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
             .retrieve()
             .bodyToMono(TasksList::class.java).map {
                 PageImpl(it.content, PageRequest.of(pageNo, pageSize), it.totalElements)
-            }.awaitSingle()
+            }.timeout(tasksClientProperties.tasksClientTimeout).awaitSingle()
     }
 
     override suspend fun getAllOpen(pageSize: Int): Flow<Page<TaskDto>> = channelFlow {
